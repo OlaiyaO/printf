dc1988172fc0d3bda0352554ccd407af520e8568
OlaiyaO <oniyajoeolaiya@gmail.com>
2023-11-17 00:58:29 -0800

    print_pointer.c hanlder

--- print_pointer.c
+++ print_pointer.c
@@ -2,33 +2,32 @@
 
 /**
  * print_pointer - Handles %p specifier (print pointer address).
- * @args: Variable arguments list
+ * @val: Variable arguments list
  *
  * Return: Number of characters printed (excluding null byte)
  */
 
-int print_pointer(va_list args)
+int print_pointer(va_list val)
 {
-	void *ptr = va_arg(args, void *);
-	int count = 0;
+	void *p = va_arg(val, void*);
+	char *nil_str = "(nil)";
+	int count;
 
-	if (ptr == NULL)
+	if (p == NULL)
 	{
-		count += write(1, "0x0", 3);
-	}
-	else
-	{
-		count += _putchar('0');
-		count += _putchar('x');
-		if ((long)ptr < 0)
+		int i;
+
+		for (i = 0; nil_str[i] != '\0'; i++)
 		{
-			count += print_number((unsigned long)((long)ptr), 16);
+			_putchar(nil_str[i]);
 		}
-		else
-		{
-			count += print_number_hex((unsigned long)ptr, 0);
-		}
+		return (i);
 	}
 
-	return (count);
+	_putchar('0');
+	_putchar('x');
+
+	count = print_number_hex((unsigned long int)p, 0);
+
+	return (count + 2);
 }
